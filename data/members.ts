212acf5215963bb044e7d8c4fa3038923f5add90
Kishan Suhi <167644864+kishys@users.noreply.github.com>
2025-07-09 23:56:29 -0400

    Technology Member name update

--- data/members.ts
+++ data/members.ts
@@ -129,6 +129,16 @@ export const departments: DepartmentType[] = [
           linkedin: "https://www.linkedin.com/in/madina-behboode-88a334270?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app ",
           instagram: "https://www.instagram.com/madina_ ",
         },
+        {
+        id: "tech-mem3",
+        name: "Ouedraogo Boukary",
+        role: "Technology Member",
+        image: "/madina.jpg",
+        bio: "Supporting the technology team with expertise in frontend development and user experience design..",
+        socialLinks: {
+          linkedin: "https://www.linkedin.com/in/madina-behboode-88a334270?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app ",
+          instagram: "https://www.instagram.com/madina_ ",
+        },
       },
     ],
   },
